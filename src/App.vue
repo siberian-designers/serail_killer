76ae3bc1252533df48503b332b836a484ec2fe54
IgorPolyakov <igorpolyakov@protonmail.com>
2021-02-04 07:16:21 +0300

    beautify & move cards

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <Card/>
-  </div>
+<div id="app">
+  <Card />
+</div>
 </template>
 
 <script>
@@ -22,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: rgb(226, 226, 226);
 }
 </style>
