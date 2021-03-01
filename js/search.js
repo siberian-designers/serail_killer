c78ca7a7ac65f2b45e143b2e90963a5b2210beb4
unknown <bazhenov570@gmail.com>
2021-03-01 12:38:06 +0700

    added search bar clearing

--- js/search.js
+++ js/search.js
@@ -1,6 +1,7 @@
 let searchBar = document.querySelector(".search_bar");
 let clearSearch = document.querySelector(".x_circle_icon");
 let elasticResults = document.querySelector(".elastic_search_container");
+let deleteSearch = document.querySelector(".x_circle_icon");
 
 searchBar.addEventListener('click', function(){
     elasticResults.classList.add('show_display');
@@ -9,8 +10,22 @@ searchBar.addEventListener('click', function(){
     popupFilter.classList.remove('open_bsh_full');
     popupFilter.classList.remove('open_bottom_sheet');
     closeFilterIcon.classList.remove('show_display');
-    touchBar.classList.remove('hide_display');
+    touchBar.classList.remove('hide_display'); 
 });
+
+searchBar.addEventListener('keyup', function(){
+    if(searchBar.value != ''){
+        deleteSearch.classList.add('show');
+    } else{
+        deleteSearch.classList.remove('show');
+    };
+});
+
+deleteSearch.addEventListener('click', function(){
+    searchBar.value = '';
+    deleteSearch.classList.remove('show');
+});
+
 
 searchBar.addEventListener('keyup', function(e){
     let key = e.keyCode;
